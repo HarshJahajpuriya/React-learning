5d2c853c2140b4da5649bb04d63972afda70e427
HarshJahajPuriya <harshkumar9826@gmail.com>
2023-01-12 22:11:01 +0530

    Example 25 - Loading Images Dynamically

---
+++ nine/src/AppExample25.js
@@ -0,0 +1,21 @@
+import React from 'react';
+const imageFolder = require.context("../public/students");
+
+const boyImage = imageFolder("./boy.jpg");
+const name = "girl1"
+const girl1 = imageFolder(`./${name}.jpg`)
+
+const AppExample25 = () => {
+  const girl3 = 'girl3';
+  return (
+    <div>
+      <h1>Example 25 - Loading Images Dynamically</h1>
+      <hr />
+      <img src={boyImage.default} />
+      <img src={girl1.default} />
+      <img src={`${process.env.PUBLIC_URL}/students/${girl3}.jpg`} />
+    </div>
+  );
+}
+
+export default AppExample25;
--- nine/src/index.js
+++ nine/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './AppExample24';
+import App from './AppExample25';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
