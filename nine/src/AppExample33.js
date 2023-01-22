81df022684ed4ef7852157444515585db4fe262f
HarshJahajPuriya <harshkumar9826@gmail.com>
2023-01-22 19:19:58 +0530

    Example 33 - @material-ui using withStyles with default statement

---
+++ nine/src/AppExample33.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core'
+
+const lookAndFeel = () => {
+  return ({
+    mainContainer: {
+      padding: '10px',
+      margin: '10px',
+      color: 'red',
+      border: '2px solid blue'
+    }
+  })
+}
+
+const AppExample33 = ({ classes }) => {
+  return (
+    <div className={classes.mainContainer}>
+      <h1>Example 33 - @material-ui using withStyles with default statement</h1>
+    </div>
+  )
+}
+
+export default withStyles(lookAndFeel)(AppExample33);
